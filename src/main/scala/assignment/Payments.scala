ae4b598803982a18fd1c381f90bafbf720b4a452
nmcb <marco.borst@splatter.nl>
2016-09-15 14:46:43 +0200

    explicitly type payments, add open questions, readability chore

--- src/main/scala/assignment/Payments.scala
+++ src/main/scala/assignment/Payments.scala
@@ -3,26 +3,45 @@ package assignment
 import scala.util.{Success, Try}
 
 class Payments(private val processedPayments: Seq[Try[Payment]]) {
+  import Payments._
 
   /** payments that could be parsed but are not validated */
-  private val parsedPayments = processedPayments.collect{ case Success(p) => p }
-
-  /** number of times a transaction reference was observed */
-  private val referenceCounters = parsedPayments
-    .map(_.transactionReference)
-    .foldLeft(Map.empty[Reference, Int]) { (rcs, ref) =>
-      rcs.updated(ref, rcs.getOrElse(ref, 0) + 1)
+  private val parsedPayments: Seq[Payment] =
+    processedPayments.collect {
+      case Success(p) => p
     }
 
+  /** number of times a transaction reference was observed */
+  private val referenceCount: ReferenceCount = {
+    val referenceCounter: (ReferenceCount, Reference) => ReferenceCount =
+      (rcs, ref) => rcs.updated(ref, rcs.getOrElse(ref, 0) + 1)
+
+    parsedPayments
+      .map(_.transactionReference)
+      .foldLeft(ReferenceCount.empty)(referenceCounter)
+  }
+
+
   /** payments that are unbalanced */
-  val unbalancedPayments = parsedPayments.filterNot(_.isBalanced)
+  val unbalancedPayments: Seq[Payment] =
+    parsedPayments.filterNot(_.isBalanced)
 
   /** payments that are ambiguously referenced */
-  val ambiguousPayments = parsedPayments.filter(p => referenceCounters(p.transactionReference) > 1)
+  val ambiguousPayments: Seq[Payment] =
+    parsedPayments.filter(p => referenceCount(p.transactionReference) > 1)
 
-  /** payments that are invalid */
-  val invalidPayments = unbalancedPayments ++ ambiguousPayments
+  /** invalid payments */
+  val invalidPayments: Seq[Payment] =
+    unbalancedPayments ++ ambiguousPayments
 
-  /** payments that are valid */
-  val validPayments = parsedPayments.filter(p => !invalidPayments.contains(p))
+  /** valid payments */
+  val validPayments =
+    parsedPayments.filter(p => !invalidPayments.contains(p))
+}
+
+object Payments {
+  type ReferenceCount = Map[Reference, Int]
+  object ReferenceCount {
+    val empty: ReferenceCount = Map.empty[Reference, Int]
+  }
 }
